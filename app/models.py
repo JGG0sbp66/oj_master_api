f23fd3cd100ebd17cec90f4ae4c5186449f9a874
KeviniLoveMei <1079705096@qq.com>
2025-04-02 20:04:37 +0800

    这个是题库页面

--- app/models.py
+++ app/models.py
@@ -5,3 +5,19 @@ class User(db.Model):
     uid = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
+
+class Questions(db.Model):
+    __tablename__ = 'questionslist'
+    u_id = db.Column(db.Integer, primary_key=True)
+    state = db.Column(db.String(80), unique=True, nullable=False)
+    title = db.Column(db.String(255), nullable=False)
+    topic = db.Column(db.String(255), nullable=False)
+    submit_num= db.Column(db.Integer, nullable=False)
+    solve_num= db.Column(db.Integer, nullable=False)
+    pass_rate = db.Column(db.Float, nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    pattern_text = db.Column(db.String(255), nullable=False)
+    print_text= db.Column(db.String(255), nullable=False)
+    test_input = db.Column(db.String(255), nullable=False)
+    test_print = db.Column(db.String(255), nullable=False)
+    first_blood = db.Column(db.String(255), nullable=False)

377d66b93634387489baf416bab347a9fc5806a9
JGG0sbp66 <2791456537@qq.com>
2025-04-03 19:03:18 +0800

    修改了配置文件，将默认数据库更改为reborn_oj_master

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 
 class Config:
     SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost:3308/oj_master'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost:3308/reborn_oj_master'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PERMANENT_SESSION_LIFETIME = timedelta(minutes=30)
     TURNSTILE_SECRET_KEY = "0x4AAAAAABC_Oa6dJZB8d7Ql7PRLdDli0Vc"

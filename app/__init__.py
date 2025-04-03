be7bb072cc3e3927a62314f55583b037546b4e6f
zq <1183681233@qq.com>
2025-04-03 22:23:47 +0800

    创建了race_info接口

--- app/__init__.py
+++ app/__init__.py
@@ -15,11 +15,13 @@ def create_app(config_class='config.Config'):  # 注意这里改为 'config.Conf
     # 延迟导入蓝图
     from .routes.auth import auth_bp
     from .routes.turnstile import turnstile_bp
-    from app.routes.questions import questions_bp
+    from .routes.questions import questions_bp
+    from .routes.race_info import race_info_bp
 
     app.register_blueprint(auth_bp, url_prefix='/api')
     app.register_blueprint(turnstile_bp, url_prefix='/api')
     app.register_blueprint(questions_bp,url_prefix='/api')
+    app.register_blueprint(race_info_bp, url_prefix='/api')
 
     app.permanent_session_lifetime = app.config['PERMANENT_SESSION_LIFETIME']
 
